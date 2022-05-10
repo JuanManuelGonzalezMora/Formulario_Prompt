717ebe6e1e58ba346b335b3d3f5cde519cb2859b
JuanManuelGonzalezMora <105092103+JuanManuelGonzalezMora@users.noreply.github.com>
2022-05-10 18:56:54 -0500

    Add files via upload

---
+++ sript.js
@@ -0,0 +1,16 @@
+let nom = prompt("Ingresa tu nombre");
+let edad = prompt("Ingresa tu edad");
+let num1 = prompt("Ingrese un numero");
+let num2 = prompt("Ingrese otro numero");
+
+let suma = parseInt(num1) + parseInt(num2);
+
+
+alert("El nombre es:  " + nom+"\nLa edad es: " + edad+"\nLos numeros ingresados son: " + num1 + " y " + num2+"\nLa suma de los numeros ingresados son: " + suma);
+
+console.log("El nombre es:  " + nom);
+console.log("La edad es: " + edad);
+console.log("Los numeros ingresados son: " + num1 + " y " + num2);
+console.log("La suma de los numeros ingresados son: " + suma);
+console.log("\n");
+console.log("FIN");
